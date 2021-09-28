2f55887a22577a93c2e22b9e75dddb1e22736e37
Evsikov <hikarokayto5@gmail.com>
2021-09-28 23:43:05 +0300

    Минимальный роутинг

---
+++ web/src/views/WelcomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="welcome">
+        <h1>Приветствие</h1>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({})
+export default class WelcomeView extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
