d263fd03c251e1e5f9b059d71c79c9739589eddb
oevsikov <oevsikov@itoolabs.com>
2021-12-09 12:16:17 +0300

    Добавление данных о юзере ( дата посещения и дата регистрации )

--- api/src/auth/dto.ts
+++ api/src/auth/dto.ts
@@ -5,9 +5,12 @@ export class RegisterDto {
     role?: string
     readonly birthday?: string
     readonly surname?: string
+    reg_date?: string
+    visit_date?: string
 }
 
 export class LoginDto {
     readonly phone: string
     readonly pass: string
+    visit_date?: string
 }
