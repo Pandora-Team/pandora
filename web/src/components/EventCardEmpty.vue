9f00b0cd882b4e6e78dfcef32be1011adcbf7495
oevsikov <oevsikov@itoolabs.com>
2021-12-09 12:32:35 +0300

    Добавление карточки ожидания мк

---
+++ web/src/components/EventCardEmpty.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="event-card event-card--empty"
+        :class="gridClass"
+    >
+        <div class="event-card__wrapper">
+            <div class="event-card__content">
+                <h2>Треня совсем скоро...</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component({})
+export default class EventCardEmpty extends Vue {
+    @Prop({ type: String, default: "" })
+    gridClass!: string
+}
+</script>
+
+<style lang="scss">
+   .event-card {
+       &--empty {
+           height: auto;
+           background: url("../assets/bg/not_mk.jpg");
+       }
+   }
+</style>
