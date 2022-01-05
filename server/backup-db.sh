13394c13c5514fab160385f98128bd81110dc1db
Himura Kazuto <glagol15@gmail.com>
2022-01-05 14:43:53 +0300

    vars

--- server/backup-db.sh
+++ server/backup-db.sh
@@ -1,5 +1,15 @@
+user=github
+password=$REPO_PASSWORD
 db_container=pandora_db_1
-bkp_path=/tmp/$db_container-$(date +'%y%m%d_%H%M%S').tgz
+archive_name="$db_container-$(date +'%y%m%d_%H%M%S').tgz"
+local_path=/tmp/$archive_name
+remote_path="$REPO_URI/$archive_name"
+
 docker exec $db_container sh -c 'rm -r /tmp/bkp* ; mongodump --username=$MONGO_INITDB_ROOT_USERNAME --password=$MONGO_INITDB_ROOT_PASSWORD --out=/tmp/bkp && tar czvf /tmp/bkp.tgz /tmp/bkp && rm -r /tmp/bkp'
-docker cp $db_container:/tmp/bkp.tgz $bkp_path
-curl -u github:$REPO_PASSWORD -T $bkp_path "$REPO_URI/$bkp_path"
+docker cp $db_container:/tmp/bkp.tgz $local_path
+curl -u github:$password -T $local_path "$remote_path"
+
+# https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-FolderInfo
+
+# https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API#ArtifactoryRESTAPI-DeleteItem
+
