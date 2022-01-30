e5acc4ad6d09d76fe2e65bb3ed83846026df4892
Evsikov <hikarokayto5@gmail.com>
2022-01-30 23:22:37 +0300

    Доделал анимацию для логотипа загрузки

--- web/src/components/LoadingView.vue
+++ web/src/components/LoadingView.vue
@@ -2,18 +2,18 @@
     <transition-loading-view>
         <div class="loading-view">
             <div class="loading-view__logo">
-<!--                <simple-svg
-                    :src="iconPath"
-                    width="94px"
+                <simple-svg
+                    :src="bgPath"
+                    width="80px"
                     height="100%"
-                />-->
+                />
                 <loading-progress
                     :indeterminate="true"
                     :counter-clockwise="false"
                     :hide-background="true"
                     :shape="path"
                     size="64"
-                    fill-duration="1"
+                    fill-duration="2"
                 />
             </div>
         </div>
@@ -34,8 +34,9 @@ import TransitionLoadingView from "@/components/transition/TransitionLoadingView
 export default class LoadingView extends Vue {
 
     path = "M58.4376 28.2741L45.9681 28.2741C45.9681 28.2741 45.2923 35.7734 48.4098 39.658C53.454 45.9437 62.8773 45.0615 67.2131 38.2742C71.1847 32.0572 69.2933 23.0928 64.0122 17.9264C56.5193 10.5963 44.1299 11.3437 38.193 19.9668C34.2882 25.6385 36.5178 37.1885 35.2591 45.9089C34.0236 54.468 24.9901 60.4831 24.9901 60.4831"
-    get iconPath(): string {
-        return require("@/assets/svg/logo.svg")
+
+    get bgPath(): string {
+        return require("@/assets/svg/logo-bg.svg")
     }
 }
 </script>
@@ -53,14 +54,23 @@ export default class LoadingView extends Vue {
         height: 100vh;
         animation: gradient 10s ease infinite alternate;
 
-        .vue-progress-path path {
-            stroke-width: 4px;
-        }
+        &__logo {
+            position: relative;
 
-        .vue-progress-path .progress {
-            stroke: white;
-        }
+            .vue-progress-path {
+                position: absolute;
+                top: -18px;
+                left: -35px;
+                path {
+                    stroke-width: 4px;
+                }
 
+                .progress {
+                    stroke: white;
+                    transition: .5s;
+                }
+            }
+        }
     }
 
     @keyframes gradient {
